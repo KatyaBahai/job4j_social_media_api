50bb451bade7466cb4e360b165865fae984e021f
KatyaBahai <katyxa-two@yandex.ru>
2025-07-17 14:43:16 +0500

    Именованные запросы [#505063]

--- src/main/java/ru/job4j/socialmediaapi/repository/PostRepository.java
+++ src/main/java/ru/job4j/socialmediaapi/repository/PostRepository.java
@@ -1,8 +1,19 @@
 package ru.job4j.socialmediaapi.repository;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
 import ru.job4j.socialmediaapi.model.Post;
 
-public interface PostRepository extends CrudRepository<Post, Long> {
+import java.awt.print.Pageable;
+import java.time.Instant;
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findByUserId(long userId);
+
+    List<Post> findByCreationDateBetween(Instant startDate, Instant endDate);
+
+    Page<Post> findByOrderByCreationDateDesc(Pageable pageable);
+
 }
