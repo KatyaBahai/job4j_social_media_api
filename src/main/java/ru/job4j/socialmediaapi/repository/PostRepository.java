3aef2c09ed2d017d9eed7d315367b4cbf0cca46c
KatyaBahai <katyxa-two@yandex.ru>
2025-07-18 14:05:15 +0500

    HQL, JPQL, Native SQL [#505064]

--- src/main/java/ru/job4j/socialmediaapi/repository/PostRepository.java
+++ src/main/java/ru/job4j/socialmediaapi/repository/PostRepository.java
@@ -2,6 +2,9 @@ package ru.job4j.socialmediaapi.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.job4j.socialmediaapi.model.Post;
 
 import java.awt.print.Pageable;
@@ -16,4 +19,37 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     Page<Post> findByOrderByCreationDateDesc(Pageable pageable);
 
+    @Modifying(clearAutomatically = true)
+    @Query("""
+            UPDATE Post p
+            SET p.description = :description, p.heading = :heading
+            WHERE p.id = :id""")
+    int updateHeadingAndDescription(
+            @Param("description") String description,
+            @Param("heading") String heading,
+            @Param("id") long id);
+
+    @Modifying(clearAutomatically = true)
+    @Query("""
+            DELETE from f File
+            WHERE f.id = :fileId""")
+    int deleteFileFromPost(@Param("fileId") long fileId);
+
+    @Modifying(clearAutomatically = true)
+    @Query("""
+            DELETE from p Post
+            WHERE p.id = :id""")
+    int deletePost(@Param("id") long id);
+
+    @Query("""
+            SELECT p from Post p
+            where p.userId IN
+            (SELECT s.followed.id
+            from Subscription s
+            WHERE s.follower.id = :followerId)
+            ORDER BY p.creationDate DESC
+            """)
+    Page<Post> findFollowedPostsByOrderByCreationDate(
+            @Param("followerId") int followerId,
+            Pageable pageable);
 }
--- src/main/java/ru/job4j/socialmediaapi/repository/UserRepository.java
+++ src/main/java/ru/job4j/socialmediaapi/repository/UserRepository.java
@@ -1,8 +1,36 @@
 package ru.job4j.socialmediaapi.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
 import ru.job4j.socialmediaapi.model.User;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    @Query("from User u WHERE u.login = :login AND u.password = :password")
+    Optional<User> findByLoginAndPassword(
+            @Param("login") String login,
+            @Param("password") String password);
+
+    @Query("""
+            SELECT s.follower
+            FROM Subscription s
+            WHERE s.followed.id = :followedId
+            AND s.isSubscribed = true
+            """)
+    List<User> findAllFollowers(@Param("followedId") long followedId);
+
+    @Query("""
+             SELECT
+                    CASE
+                        WHEN f.friend1.id = :friendId THEN f.friend2
+                        ELSE f.friend1
+                    END
+                FROM Friendship f
+                WHERE f.friend1.id = :friendId OR f.friend2.id = :friendId
+            """)
+    List<User> findAllFriends(@Param("friendId") long friendId);
 }
